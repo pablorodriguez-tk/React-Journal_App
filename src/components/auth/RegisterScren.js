17628540479e7de0488f68e29c86bfd41bdf06ea
Pablo Rodriguez <pablorodriguez88@gmail.com>
2021-05-05 00:01:18 -0300

    Add register and login scss

--- src/components/auth/RegisterScren.js
+++ src/components/auth/RegisterScren.js
@@ -1,10 +1,46 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const RegisterScren = () => {
   return (
-    <div>
-      <h1>RegisterScren</h1>
-    </div>
+    <>
+      <h3 className="auth__title">Register</h3>
+      <form>
+        <input
+          className="auth__input"
+          type="text"
+          placeholder="Name"
+          name="Name"
+          autoComplete="off"
+        />
+        <input
+          className="auth__input"
+          type="text"
+          placeholder="Email"
+          name="email"
+          autoComplete="off"
+        />
+        <input
+          className="auth__input"
+          type="password"
+          placeholder="Password"
+          name="password"
+        />
+        <input
+          className="auth__input"
+          type="password"
+          placeholder="Confirm password"
+          name="password2"
+        />
+        <button className="btn btn-primary btn-block mb-5" type="submit">
+          Register
+        </button>
+
+        <Link className="link" to="auth/login">
+          Already registered?
+        </Link>
+      </form>
+    </>
   );
 };
 
--- src/routers/AuthRouter.js
+++ src/routers/AuthRouter.js
@@ -5,11 +5,15 @@ import RegisterScren from "../components/auth/RegisterScren";
 
 const AuthRouter = () => {
   return (
-    <Switch>
-      <Route exact path="/auth/login" component={LoginScreen} />
-      <Route exact path="/auth/register" component={RegisterScren} />
-      <Redirect to="/auth/login" />
-    </Switch>
+    <div className="auth__main">
+      <div className="auth__box-container">
+        <Switch>
+          <Route exact path="/auth/login" component={LoginScreen} />
+          <Route exact path="/auth/register" component={RegisterScren} />
+          <Redirect to="/auth/login" />
+        </Switch>
+      </div>
+    </div>
   );
 };
 
