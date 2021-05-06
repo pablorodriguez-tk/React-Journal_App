8a5ede46f218816c9c30f74343d06f2c103940c3
Pablo Rodriguez <pablorodriguez88@gmail.com>
2021-05-06 15:28:42 -0300

    Add private and public routes, and implent logout

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -54,7 +54,14 @@ export const login = (uid, displayName) => ({
   },
 });
 
+export const startLogout = () => {
+  return async (dispatch) => {
+    await firebase.auth().signOut();
+
+    dispatch(logout());
+  };
+};
+
 export const logout = () => ({
   type: types.logout,
-  payload: {},
 });
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,20 +1,50 @@
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-} from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Switch, Redirect } from "react-router-dom";
 import JournalScreen from "../components/journal/JournalScreen";
 import AuthRouter from "./AuthRouter";
+import { firebase } from "../firebase/firebase-config";
+import { useDispatch } from "react-redux";
+import { login } from "../actions/auth";
+import PublicRoute from "./PublicRoute";
+import PrivateRoute from "./Private.Route";
 
 const AppRouter = () => {
+  const dispatch = useDispatch();
+  const [checking, setChecking] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    //Mantiene el estado de la autenticacion al recargar la pagina
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user?.uid) {
+        dispatch(login(user.uid, user.displayName));
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setChecking(false);
+    });
+  }, [dispatch, setChecking, setIsLoggedIn]);
+
+  if (checking) {
+    return <h1>Espere...</h1>;
+  }
+
   return (
     <Router>
       <div>
         <Switch>
-          <Route path="/auth" component={AuthRouter} />
-          <Route exact path="/" component={JournalScreen} />
+          <PublicRoute
+            path="/auth"
+            component={AuthRouter}
+            isAuthenticated={isLoggedIn}
+          />
+          <PrivateRoute
+            exact
+            path="/"
+            component={JournalScreen}
+            isAuthenticated={isLoggedIn}
+          />
 
           <Redirect to="/auth/login" />
         </Switch>
