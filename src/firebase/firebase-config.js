4430c8584136fa14f02208495375036ed3238e7f
Pablo Rodriguez <pablorodriguez88@gmail.com>
2021-05-08 12:33:41 -0300

    Add test

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -3,32 +3,15 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDRhct52D-DgsMgwGR545k0QpV-sUCiReA",
-  authDomain: "react-journal-app-a6891.firebaseapp.com",
-  projectId: "react-journal-app-a6891",
-  storageBucket: "react-journal-app-a6891.appspot.com",
-  messagingSenderId: "674763713061",
-  appId: "1:674763713061:web:1c3a9a22d70bb394d0edcc",
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID,
 };
 
-const firebaseConfigTesting = {
-  apiKey: "AIzaSyBQtWuhfYx3ykQs3ShuYj5jKXP__ogG2Rw",
-  authDomain: "sql-demos-e1c6b.firebaseapp.com",
-  databaseURL: "https://sql-demos-e1c6b-default-rtdb.firebaseio.com",
-  projectId: "sql-demos-e1c6b",
-  storageBucket: "sql-demos-e1c6b.appspot.com",
-  messagingSenderId: "961257930460",
-  appId: "1:961257930460:web:0c95887698b3d034c4be19",
-  measurementId: "G-R1YWZZ8CVT",
-};
-
-if (process.env.NODE_ENV === "test") {
-  //test
-  firebase.initializeApp(firebaseConfigTesting);
-} else {
-  //dev / prod
-  firebase.initializeApp(firebaseConfig);
-}
+firebase.initializeApp(firebaseConfig);
 
 const db = firebase.firestore();
 const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
